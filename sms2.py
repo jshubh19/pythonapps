50a05684f9405cb9c665142e768e5bf6dc28f73b
Shubham Jangid <34124343+jshubh19@users.noreply.github.com>
2019-03-06 13:01:46 +0530

    Update sms2.py

--- sms2.py
+++ sms2.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
 
-account_sid = "AC561b21ca46eab7cd51348514510099f1"
+account_sid = "ac sid"
 
-auth_token = "aec870ccc4f26e3ea808a46cd289a4fc"
+auth_token = "here is your twilio auth token"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+918094823877",
+    to="mobile number",
     from_="+1 858-281-4781 ",
     media_url='https://en.facebookbrand.com/wp-content/uploads/2016/05/FB-fLogo-Blue-broadcast-2.png'
     )
